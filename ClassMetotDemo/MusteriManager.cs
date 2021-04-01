00c29305dc7ce6fba37a4d7e0f817889e524e8f8
salihkiyici <salihkiyici852@gmail.com>
2021-04-01 12:07:44 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi +" "+ "isimli kullanıcı sisteme başarıyla eklendi.");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + "isimli kullanıcı sistemden başarıyla silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 001;
+            musteri1.MusteriAdi = "Kemal";
+            musteri1.MusteriSoyadi = "Yılmaz";
+            musteri1.TcKimlikNo = "12132546567";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 002;
+            musteri2.MusteriAdi = "Yiğit";
+            musteri2.MusteriSoyadi = "Kahraman";
+            musteri2.TcKimlikNo = "65464651657";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+        }
+    }
+}
